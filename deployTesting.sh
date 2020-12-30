5250bcc9f87d2cfcc3ed07c21cd3cc10dd01ce27
twittingeek <palma.giacomo@gmail.com>
2020-12-30 16:51:05 +0100

    ignore

--- deployTesting.sh
+++
@@ -1,6 +0,0 @@
- #!/bin/bash
- echo "#### Deploying testing ####"
- rsync -auv --exclude 'vendor' --exclude='bin' * root@139.162.151.157:/root/testing.webProbeApi
- echo "#### Running tests ####"
- ssh root@139.162.151.157 'cd testing.webProbeApi && ./bin/phpunit'
- echo "#### Tests over ####"
