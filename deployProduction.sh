f19fa26cb67ef5d6d59511fb87adcf9c4a7fad51
twittingeek <palma.giacomo@gmail.com>
2020-12-30 16:15:40 +0100

    [12-EVALUATORINTERFACE] deploy

--- deployProduction.sh
+++ deployProduction.sh
@@ -1,13 +1,15 @@
  #!/bin/bash
- echo #### Deploying testing ####
+ echo "#### Deploying testing ####"
  rsync -auv --exclude 'vendor' --exclude='bin' * root@139.162.151.157:/root/testing.webProbeApi
- echo #### Running tests ####
+ echo "#### Running tests ####"
  ssh root@139.162.151.157 'cd testing.webProbeApi && ./bin/phpunit'
- echo #### Tests over ####
+ echo "#### Tests over ####"
  read -p "Do you want to deploy production now? " -n 1 -r
  echo    # (optional) move to a new line
  if [[ $REPLY =~ ^[Yy]$ ]]
  then
- 	 echo #### Deploying production
+ 	 echo "#### Deploying production ####"
      rsync -auv --exclude 'vendor' --exclude='bin' * root@139.162.151.157:/root/webProbeApi
+     ssh root@139.162.151.157 'cd webProbeApi && ./bin/phpunit'
  fi
+ echo "#### done ####"
--- deployTesting.sh
+++ deployTesting.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
- echo #### Deploying testing ####
+ echo "#### Deploying testing ####"
  rsync -auv --exclude 'vendor' --exclude='bin' * root@139.162.151.157:/root/testing.webProbeApi
- echo #### Running tests ####
+ echo "#### Running tests ####"
  ssh root@139.162.151.157 'cd testing.webProbeApi && ./bin/phpunit'
- echo #### Tests over ####
+ echo "#### Tests over ####"
